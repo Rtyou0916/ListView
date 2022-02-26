9e78232a5510ddca39b2fc1a50d19d16dee61b72
최다영 <rtyou@naver.com>
2022-02-26 15:01:30 +0900

    Student Adapter가 세가지 재료를 받아오도록 설정> 부모클래스 ArrayAdapter에게 전달

--- app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
@@ -1,9 +1,16 @@
 package com.example.listview.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.example.listview.datas.StudentData
 
-class StudentAdapter: ArrayAdapter<StudentAdapter>() {
+class StudentAdapter(
+    val mContext: Context,  //어느 화면에서?
+    val resId: Int,  // 어떤 xml을 기반으로 그려줄거니?
+    val mList: ArrayList<StudentData> // 실제로 뿌려줄 학생들은 누구니, 목록
+
+
+): ArrayAdapter<StudentAdapter>(mContext,resId,mList) {
 
 
 
