6c54762a9f0e784211ffc462d88daf1bd7fb61ea
최다영 <rtyou@naver.com>
2022-02-26 15:12:29 +0900

    Studentlist_item xml 노트를 코틀린에서 활용하도록 함

--- app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview.R
 import com.example.listview.datas.StudentData
 
 class StudentAdapter(
@@ -9,11 +13,17 @@ class StudentAdapter(
     val resId: Int,  // 어떤 xml을 기반으로 그려줄거니?
     val mList: ArrayList<StudentData> // 실제로 뿌려줄 학생들은 누구니, 목록
 
+    ) : ArrayAdapter<StudentAdapter>(mContext, resId, mList) {
 
-): ArrayAdapter<StudentAdapter>(mContext,resId,mList) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
 
 
-
-
+        }
+    }
 
 }
