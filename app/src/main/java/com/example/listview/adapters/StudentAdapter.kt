857969b8bc94bb502ac2a26f0b4616c4fc7ddd36
최다영 <rtyou@naver.com>
2022-02-26 14:56:48 +0900

    학생 어댑터 클래스

---
+++ app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.listview.adapters
+
+import android.widget.ArrayAdapter
+import com.example.listview.datas.StudentData
+
+class StudentAdapter: ArrayAdapter<StudentAdapter>() {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/listview/datas/StudentData.kt
@@ -0,0 +1,13 @@
+package com.example.listview.datas
+
+class StudentData (
+    val name: String,
+    val birthYear: Int,
+    val address: String,
+//    마지막이라도 , 찍어도 괜찮음. 요즘 문법. 자바 스크립트와 파이썬 등도 지원.
+    ){
+
+
+
+
+}
